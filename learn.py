31f63ed618e53bfc5afa563be92589f33fbbb34b
abhishek12aug <abhishek.aug122001@gmail.com>
2025-04-17 16:09:59 +0530

    learn.py

---
+++ learn.py
@@ -0,0 +1,14 @@
+# sum of digits present inside a string 
+
+a = input("enter anything : ")  
+b = 5 
+total = 0 
+for char in a: 
+    # ASCII values to int value 
+    num = ord(char) - ord('0')
+    total = total + num
+
+print(total)
+
+
+
